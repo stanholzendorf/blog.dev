c007c48017d7d05ad7138587f21bc477c340cc42
stanholzendorf <sholzendorf@mail.com>
2016-06-05 23:35:06 -0500

    Small correction

--- app/models/BaseModel.php
+++ app/models/BaseModel.php
@@ -25,8 +25,8 @@ class BaseModel extends Eloquent
         'from' => 'required|regex:/^[(a-zA-Z\s)]+$/u|min:3|max:32',
         'email' => 'required|email|',
         'phonenumber' => 'required|numeric|min:5',
-        'budget' => 'required|alpha_num',
-        'address' => 'required|min:9|max:32',
+        'budget' => 'required|regex:/^[(a-zA-Z\s)]+$/u',
+        'address' => 'required|min:9|max:600',
         'body' => 'required|min:3|max:32'
     );
 
